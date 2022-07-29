0ceb27d3bbe6ed10d0f0fb64d2ea184bff66605b
vaghasiyapiyush <104962169+vaghasiyapiyush@users.noreply.github.com>
2022-07-29 14:27:10 +0530

    Add files via upload

---
+++ Events.js
@@ -0,0 +1,23 @@
+function demoOnClick(){
+    alert("On Click");
+}
+
+function demoOnDblClick(){
+    alert("On Double Click");
+}
+
+function demoOnFocus(){
+    document.getElementById("txtFocus").style.backgroundColor="Red";
+}
+
+function demoOnBlur(){
+    document.getElementById("txtBlur").style.backgroundColor="yellow";
+}
+
+function demoOnKeypress(){
+    alert("Keypress Done");
+}
+
+function demoOnKeyUp(){
+    alert("KeyUp Done");
+}
